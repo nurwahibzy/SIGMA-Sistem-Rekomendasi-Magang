a62eb255977aab13891b29b691b4fa5020eb4aee
aidas777-ctrl <fadhila2903@gmail.com>
2025-05-16 11:22:11 +0700

    mhs

--- app/Http/Controllers/Mahasiswa/MagangController.php
+++ app/Http/Controllers/Mahasiswa/MagangController.php
@@ -25,18 +25,22 @@ class MagangController extends Controller
             ->id_mahasiswa;
         return $id_mahasiswa;
     }
-    public function getDashboard()
-    {
-        $magang = PeriodeMagangModel::with(
-            'lowongan_magang:id_lowongan,id_perusahaan,id_bidang,nama,foto_path',
-            'lowongan_magang.perusahaan:id_perusahaan,id_jenis,nama',
-            'lowongan_magang.bidang:id_bidang,nama',
-            'lowongan_magang.perusahaan.jenis_perusahaan:id_jenis,jenis'
-        )
-            ->where('tanggal_mulai', '>', now())
-            ->get(['id_lowongan', 'tanggal_mulai', 'tanggal_selesai']);
-        return response()->json($magang);
-    }
+  public function getDashboard()
+{
+    $magang = PeriodeMagangModel::with(
+        'lowongan_magang:id_lowongan,id_perusahaan,id_bidang,nama,foto_path',
+        'lowongan_magang.perusahaan:id_perusahaan,id_jenis,nama',
+        'lowongan_magang.bidang:id_bidang,nama',
+        'lowongan_magang.perusahaan.jenis_perusahaan:id_jenis,jenis'
+    )
+        ->where('tanggal_mulai', '>', now())
+        ->get(['id_lowongan', 'tanggal_mulai', 'tanggal_selesai']);
+
+    return view('mahasiswa.index', [
+        'magang' => $magang
+    ]);
+}
+
 
     public function getMagang($id_periode)
     {
---
+++ resources/views/layouts/tamplate.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGMA</title>
+
+    <link rel="shortcut icon" href="{{ asset('template/assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/app-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/iconly.css') }}">
+</head>
+
+<body>
+    <script src="{{ asset('template/assets/static/js/initTheme.js') }}"></script>
+
+    <div id="app">
+        <div id="main" class="layout-horizontal">
+
+            {{-- Header --}}
+            @include('layouts.header')
+
+            {{-- Sidebar --}}
+            @include('layouts.sidebar')
+
+            {{-- Main content --}}
+            <div class="page-content">
+                <div class="container">
+                    @yield('content')
+                </div>
+            </div>
+
+            {{-- Footer --}}
+            @include('layouts.footer')
+
+        </div>
+    </div>
+</body>
+</html>
