99d52755d9f53082d5e02df6b7dd2eb40ca8e3d7
tjahjoe <ntell634@gmail.com>
2025-05-19 06:29:43 +0700

    add: update magang from admnin

--- app/Http/Controllers/Admin/MagangController.php
+++ app/Http/Controllers/Admin/MagangController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\MagangModel;
 use Illuminate\Http\Request;
 
 class MagangController extends Controller
 {
-    public function getDashboard(){
+    public function getDashboard()
+    {
         return view('welcome');
         // return response()->json('a');
+    }
+
+    public function putMagang(Request $request, $id_magang)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $id_dosen = $request->input('id_dosen');
+            $status = $request->input('status');
+
+            MagangModel::where('id_magang', $id_magang)
+                ->update([
+                    'id_dose' => $id_dosen,
+                    'status' => $status
+                ]);
+        }
     }
 }
