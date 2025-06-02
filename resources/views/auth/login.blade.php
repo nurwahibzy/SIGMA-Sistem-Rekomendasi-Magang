1a4f91235a9b26e82d47bbd71a5059802fffdc2b
saka <sakanabil1221@gmail.com>
2025-06-02 12:49:35 +0700

    fix: login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,6 +13,19 @@
     <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/app-dark.css') }}">
     <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/iconly.css') }}">
     <link rel="stylesheet" href="{{ asset('template/assets/compiled/css/auth.css') }}">
+    <style>
+    html, body {
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+    }
+
+    #auth {
+        height: 100vh;
+        overflow: hidden;
+    }
+</style>
+
 </head>
 
 <body>
@@ -20,13 +33,13 @@
         <div class="row h-100">
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
-                    <div class="auth-logo">
+                    <div class="auth-logo" style="margin-bottom: 40px">
                         <a href="{{ url('index.html') }}">
                             <img src="{{ asset('template/assets/compiled/svg/logo.svg') }}" alt="Logo"  style="width: 200px; height: auto;">
                         </a>
                     </div>
-                    <h1 class="auth-title">Login</h1>
-                    <p class="auth-subtitle mb-5">Silakan login menggunakan data yang sesuai.</p>
+                    <h2 class="auth-title">Login</h2>
+                    <p class="auth-subtitle mb-1">Silakan login menggunakan data yang sesuai.</p>
 
                     <form action="{{ url('login') }}" method="POST" id="login">
                         @csrf
