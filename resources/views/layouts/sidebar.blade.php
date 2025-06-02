266de5802eeefeedc74b55a2ed98a7ba77923b16
saka <sakanabil1221@gmail.com>
2025-06-02 10:34:40 +0700

    fix: sidebar dosen

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -149,14 +149,7 @@
             <div class="sidebar-menu">
                 <ul class="menu">
                     <li class="sidebar-title">Menu</li>
-
-                    <li class="sidebar-item {{ request()->is('dosen/dashboard*') ? 'active' : '' }}">
-                        <a href="{{ url('/dosen/dashboard') }}" class="sidebar-link">
-                            <i class="bi bi-grid-fill"></i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-
+                    
                     <li class="sidebar-item {{ request()->is('dosen/riwayat*') ? 'active' : '' }}">
                         <a href="{{ url('/dosen/riwayat') }}" class="sidebar-link">
                             <i class="bi bi-people-fill"></i>
